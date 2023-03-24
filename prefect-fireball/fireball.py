a32b31b5e7b69074d9b98b3919c4fddf0d0e90dc
tpdorsey <terrend@mishu.com>
2023-03-23 22:15:01 -0400

    remove block name

--- prefect-fireball/fireball.py
+++ prefect-fireball/fireball.py
@@ -9,6 +9,7 @@ api_url = 'https://ssd-api.jpl.nasa.gov/fireball.api?limit=5'
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36'
 }
+notification_block = ""
 
 @task(retries=3, retry_delay_seconds=10)
 def get_data_from_api():
@@ -49,7 +50,7 @@ def log_fireballs(df):
 @task
 def notify_fireballs(df):
     fireballs = df.to_dict(orient="records")
-    slack_webhook = SlackWebhook.load("slack-flow-alerts")
+    slack_webhook = SlackWebhook.load(notification_block)
     
     logger = get_run_logger()
     for fb in fireballs:
